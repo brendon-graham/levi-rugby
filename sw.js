5dc4638c14342fde76a0d3c308d54c11bec743e8
brendon-graham <BG@pfe.nz>
2026-07-21 06:49:14 +1200

    fix: v0.7.2 training-day swap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'levi-rugby-v0.7.1';
+const CACHE = 'levi-rugby-v0.7.2';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))));
